e80d0d764318c5561660a3e0c9ebeb7804da3f84
Connor Ross <cross@mdsol.com>
2014-11-03 23:48:59 -0500

    created a fully working circuit breaker library

--- src/CircuitBreaker.cs
+++ src/CircuitBreaker.cs
@@ -6,53 +6,236 @@ using System.Threading;
 
 namespace CircuitBreakerDotNet
 {
-    public class CircuitBreaker
+    public enum CircuitBreakerStateEnum
+    {
+        Closed,
+        Open,
+        HalfOpen
+    }
+
+    public interface ICircuitBreakerState
+    {
+        CircuitBreakerStateEnum State { get; }
+        DateTime LastStateChangedDateUtc { get; }
+        Exception LastException { get; }
+        bool IsClosed { get; }
+    }
+
+    public interface ICircuitBreakerStateStore : ICircuitBreakerState
+    {
+        void Trip(Exception exception);
+        void Reset();
+        void HalfOpen();
+
+    }
+
+    public class CircuitBreakerStateStore : ICircuitBreakerStateStore
+    {
+        private CircuitBreakerStateEnum _State;
+        private Exception _LastException;
+        private DateTime _LastStateChangedDateUtc;
+
+        public CircuitBreakerStateStore()
+        {
+            _State = CircuitBreakerStateEnum.Closed;
+            _LastException = null;
+            _LastStateChangedDateUtc = DateTime.MinValue;
+        }
+
+
+        public CircuitBreakerStateEnum State
+        {
+            get { return _State; }
+        }
+
+        public DateTime LastStateChangedDateUtc
+        {
+            get { return _LastStateChangedDateUtc; }
+        }
+
+        public Exception LastException
+        {
+            get { return _LastException; }
+        }
+
+        public bool IsClosed
+        {
+            get { return _State == CircuitBreakerStateEnum.Closed; }
+        }
+
+        public void Trip(Exception exception)
+        {
+            _State = CircuitBreakerStateEnum.Open;
+            _LastException = exception;
+            _LastStateChangedDateUtc = DateTime.UtcNow;
+        }
+
+        public void Reset()
+        {
+            _State = CircuitBreakerStateEnum.Closed;
+            _LastException = null;
+            _LastStateChangedDateUtc = DateTime.UtcNow;
+        }
+
+        public void HalfOpen()
+        {
+            _State = CircuitBreakerStateEnum.HalfOpen;
+            _LastStateChangedDateUtc = DateTime.UtcNow;
+        }
+    }
+
+    public interface ICircuitBreakerClosedToOpen
+    {
+        bool ExceptionThrownWhileCircuitClosedShouldTripCircuit(Exception exceptionThrown, ICircuitBreakerState currentState);
+        void ActionExecutedSuccessfullyWhileCircuitClosed(ICircuitBreakerState currentState);
+    }
+
+    public class FailCountBasedCircuitBreakerClosedToOpen : ICircuitBreakerClosedToOpen
     {
         private readonly int _FailLimit;
+        private readonly TimeSpan _TimeToRememberFailures;
 
         private int _FailCount;
-        private Exception _LastException;
+        private DateTime _LastFailTimeUTC;
 
-        public CircuitBreaker(int failLimit)
+        public FailCountBasedCircuitBreakerClosedToOpen(int failLimit, TimeSpan timeToRememberFailures)
         {
             _FailLimit = failLimit;
+            _TimeToRememberFailures = timeToRememberFailures;
             _FailCount = 0;
+        }
+
+        public bool ExceptionThrownWhileCircuitClosedShouldTripCircuit(Exception exceptionThrown, ICircuitBreakerState currentState)
+        {
+            Interlocked.Increment(ref _FailCount);
+            var shouldTripCircuit = Interlocked.Equals(_FailCount, _FailLimit);
+            _LastFailTimeUTC = DateTime.UtcNow;
+
+            return shouldTripCircuit;
+        }
+
+        public void ActionExecutedSuccessfullyWhileCircuitClosed(ICircuitBreakerState currentState)
+        {
+            if (_FailCount == 0 || _LastFailTimeUTC + _TimeToRememberFailures > DateTime.UtcNow) 
+                return;
+
+            Interlocked.Exchange(ref _FailCount, 0);
+        }
+    }
+
+    public interface ICircuitBreakerOpenToClosed
+    {
+        bool CircuitOpenIsCircuitReadyToHalfOpen(ICircuitBreakerState currentState);
+    }
+
+    public class WaitTimeBasedCircuitBreakerOpenToClosed : ICircuitBreakerOpenToClosed
+    {
+        private readonly TimeSpan _OpenRetryWaitTime;
+
+        public WaitTimeBasedCircuitBreakerOpenToClosed(TimeSpan openRetryWaitTime)
+        {
+            _OpenRetryWaitTime = openRetryWaitTime;
+        }
+
+        public bool CircuitOpenIsCircuitReadyToHalfOpen(ICircuitBreakerState currentState)
+        {
+            var isReadyToHalfOpen = currentState.LastStateChangedDateUtc + _OpenRetryWaitTime < DateTime.UtcNow;
+            return isReadyToHalfOpen;
+        }
+    }
+
+    public class CircuitBreaker
+    {
+        private readonly ICircuitBreakerStateStore _StateStore;
+        private readonly ICircuitBreakerClosedToOpen _ClosedToOpen;
+        private readonly ICircuitBreakerOpenToClosed _OpenToClosed;
+        private readonly object _HalfOpenSyncObject;
+
+        public CircuitBreaker(ICircuitBreakerStateStore stateStore, ICircuitBreakerClosedToOpen closedToOpen, ICircuitBreakerOpenToClosed openToClosed)
+        {
+            _StateStore = stateStore;
+            _ClosedToOpen = closedToOpen;
+            _OpenToClosed = openToClosed;
+            _HalfOpenSyncObject = new object();
         }
 
         public void ExecuteAction(Action action)
         {
-            if (FailLimitReached())
+            if (IsOpen())
             {
+                if (IsReadyToHalfOpen())
+                {
+                    var lockTaken = false;
+                    try
+                    {
+                        lockTaken = Monitor.TryEnter(_HalfOpenSyncObject);
+                        if (lockTaken)
+                        {
+                            action();
+
+                            _StateStore.Reset();
+                            return;
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        _StateStore.Trip(exception);
+                        throw;
+                    }
+                    finally
+                    {
+                        if (lockTaken)
+                        {
+                            Monitor.Exit(_HalfOpenSyncObject);
+                        }
+                    }
+                }
                 throw CreateCircuitOpenException();
             }
 
             try
             {
                 action();
+                _ClosedToOpen.ActionExecutedSuccessfullyWhileCircuitClosed(_StateStore);
             }
             catch (Exception exception)
             {
-                Interlocked.Increment(ref _FailCount);
-                _LastException = exception;
-
-                if (FailLimitReached())
-                {
-                    throw CreateCircuitOpenException();
-                }
+                TrackException(exception);
 
                 throw;
             }
         }
 
-        private CircuitBreakerOpenException CreateCircuitOpenException()
+        private bool IsReadyToHalfOpen()
         {
-            return new CircuitBreakerOpenException(_LastException);
+            var isReadyToHalfOpen = _OpenToClosed.CircuitOpenIsCircuitReadyToHalfOpen(_StateStore);
+            return isReadyToHalfOpen;
         }
 
-        private bool FailLimitReached()
+        private void TrackException(Exception exception)
         {
-            var result = Interlocked.Equals(_FailCount, _FailLimit);
-            return result;
+            var shouldTripCiruit = ExceptionThrownWhileCircuitClosedShouldTripCircuit(exception);
+
+            if (shouldTripCiruit)
+            {
+                _StateStore.Trip(exception);
+            }
+        }
+
+        private bool ExceptionThrownWhileCircuitClosedShouldTripCircuit(Exception exception)
+        {
+            var shouldTripCircuit = _ClosedToOpen.ExceptionThrownWhileCircuitClosedShouldTripCircuit(exception, _StateStore);
+            return shouldTripCircuit;
+        }
+
+        private bool IsOpen()
+        {
+            return !_StateStore.IsClosed;
+        }
+
+        private CircuitBreakerOpenException CreateCircuitOpenException()
+        {
+            return new CircuitBreakerOpenException(_StateStore.LastException);
         }
     }
 
--- test/CircuitBreakerTest/CircuitBreakerShould.cs
+++ test/CircuitBreakerTest/CircuitBreakerShould.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CircuitBreakerDotNet;
+using System.Threading;
 
 namespace CircuitBreakerTest
 {
     [TestClass]
     public class CircuitBreakerShould
     {
-        int _FailLimit = 2;
+        static int _FailLimit = 2;
+        static TimeSpan _WaitTime = new TimeSpan(0, 0, 0, 0, 3);
+        ICircuitBreakerStateStore _CircuitBreakerStateStore;
         CircuitBreaker _CircuitBreaker;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            _CircuitBreaker = new CircuitBreaker(_FailLimit);
+            _CircuitBreakerStateStore = new CircuitBreakerStateStore();
+            var closedToOpen = new FailCountBasedCircuitBreakerClosedToOpen(_FailLimit, _WaitTime);
+            var openToClosed = new WaitTimeBasedCircuitBreakerOpenToClosed(_WaitTime);
+            _CircuitBreaker = new CircuitBreaker(_CircuitBreakerStateStore, closedToOpen, openToClosed);
         }
 
         [TestMethod]
@@ -33,33 +39,25 @@ namespace CircuitBreakerTest
         [ExpectedException(typeof(DivideByZeroException))]
         public void ThrowActionsException()
         {
-            _CircuitBreaker.ExecuteAction(() =>
-            {
-                var zero = 0;
-                zero = zero / zero;
-            });
+            _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
         }
 
         [TestMethod]
         public void ThrowCircuitBreakerOpenExceptionWhenActionFailsOverFailLimit()
         {
-            Action divideByZeroAction = () =>
-                {
-                    var zero = 0;
-                    zero = zero / zero;
-                };
-            for (var i = 0; i < (_FailLimit - 1); i++)
+
+            for (var i = 0; i < _FailLimit; i++)
             {
                 try
                 {
-                    _CircuitBreaker.ExecuteAction(divideByZeroAction);
+                    _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
                 }
                 catch (DivideByZeroException) { }
             }
 
             try
             {
-                _CircuitBreaker.ExecuteAction(divideByZeroAction);
+                _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
                 Assert.Fail("Should have thrown CircuitBreakerOpenException");
             }
             catch (CircuitBreakerOpenException exception)
@@ -73,23 +71,7 @@ namespace CircuitBreakerTest
         {
             var executed = false;
 
-            Action divideByZeroAction = () =>
-            {
-                var zero = 0;
-                zero = zero / zero;
-            };
-            for (var i = 0; i < (_FailLimit); i++)
-            {
-                try
-                {
-                    _CircuitBreaker.ExecuteAction(divideByZeroAction);
-                }
-                catch (DivideByZeroException) { }
-                catch (CircuitBreakerOpenException exception)
-                {
-                    Assert.IsInstanceOfType(exception.LastExceptionThatTrippedCircuit, typeof(DivideByZeroException));
-                }
-            }
+            MoveCircuitBreakerToOpenState();
 
             try
             {
@@ -99,6 +81,77 @@ namespace CircuitBreakerTest
             catch (CircuitBreakerOpenException) { }
 
             Assert.IsFalse(executed);
+        }
+
+        [TestMethod]
+        public void RetryActionAfterBeingOpenForWaitTimeAndCloseIfSuccessful()
+        {
+            MoveCircuitBreakerToOpenState();
+            Thread.Sleep(_WaitTime.Add(new TimeSpan(0,0,0,0,3)));
+
+            var tried = false;
+            _CircuitBreaker.ExecuteAction(() => tried = true);
+
+            Assert.IsTrue(tried);
+            Assert.AreEqual(CircuitBreakerStateEnum.Closed, _CircuitBreakerStateStore.State);
+        }
+
+        [TestMethod]
+        public void RetryActionAfterBeingOpenForWaitTimeAndGoBackToOpenIfFailed()
+        {
+            MoveCircuitBreakerToOpenState();
+            Thread.Sleep(_WaitTime.Add(new TimeSpan(0, 0, 0, 0, 3)));
+
+            try
+            {
+                _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
+                Assert.Fail("Should have thrown DivideByZeroException");
+            }
+            catch (DivideByZeroException) { }
+
+            Assert.AreEqual(CircuitBreakerStateEnum.Open, _CircuitBreakerStateStore.State);
+        }
+
+
+
+        [TestMethod]
+        public void ForgetFailuresAfterAConfiguredTimeWhenSuccessHappens()
+        {
+            // Get right up to the fail limit
+            for (var i = 0; i < (_FailLimit - 1); i++)
+            {
+                try
+                {
+                    _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
+                }
+                catch (DivideByZeroException) { }
+            }
+
+            // sleep some time
+            Thread.Sleep(_WaitTime.Add(new TimeSpan(0, 0, 0, 0, 3)));
+
+            // success
+            _CircuitBreaker.ExecuteAction(() => { });
+
+            try
+            {
+                _CircuitBreaker.ExecuteAction(DivideByZeroExceptionAction);
+                Assert.Fail("Should have thrown DivideByZeroException");
+            }
+            catch (DivideByZeroException) { }
+
+            Assert.AreEqual(CircuitBreakerStateEnum.Closed, _CircuitBreakerStateStore.State);
+        }
+
+        private static void DivideByZeroExceptionAction()
+        {
+            var zero = 0;
+            zero = zero / zero;
+        }
+
+        private void MoveCircuitBreakerToOpenState()
+        {
+            _CircuitBreakerStateStore.Trip(new DivideByZeroException());
         }
     }
 }
