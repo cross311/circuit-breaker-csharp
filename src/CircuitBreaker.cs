a2e84e818abd53349e2e66ad2866ea4748c2ab4b
Connor Ross <cross@mdsol.com>
2014-11-03 22:04:43 -0500

    starting work on circuit breaker code

---
+++ src/CircuitBreaker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CircuitBreakerDotNet
+{
+    public class CircuitBreaker
+    {
+        private readonly int _FailLimit;
+
+        private int _FailCount;
+        private Exception _LastException;
+
+        public CircuitBreaker(int failLimit)
+        {
+            _FailLimit = failLimit;
+            _FailCount = 0;
+        }
+
+        public void ExecuteAction(Action action)
+        {
+            if (FailLimitReached())
+            {
+                throw CreateCircuitOpenException();
+            }
+
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                Interlocked.Increment(ref _FailCount);
+                _LastException = exception;
+
+                if (FailLimitReached())
+                {
+                    throw CreateCircuitOpenException();
+                }
+
+                throw;
+            }
+        }
+
+        private CircuitBreakerOpenException CreateCircuitOpenException()
+        {
+            return new CircuitBreakerOpenException(_LastException);
+        }
+
+        private bool FailLimitReached()
+        {
+            var result = Interlocked.Equals(_FailCount, _FailLimit);
+            return result;
+        }
+    }
+
+    public class CircuitBreakerOpenException : Exception
+    {
+        private readonly Exception _LastExceptionThatTrippedCircuit;
+
+        public CircuitBreakerOpenException(Exception lastExceptionThatTrippedCircuit)
+        {
+            _LastExceptionThatTrippedCircuit = lastExceptionThatTrippedCircuit;
+        }
+
+        public Exception LastExceptionThatTrippedCircuit
+        {
+            get
+            {
+                return _LastExceptionThatTrippedCircuit;
+            }
+        }
+    }
+}
---
+++ test/CircuitBreakerTest/CircuitBreakerShould.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CircuitBreakerDotNet;
+
+namespace CircuitBreakerTest
+{
+    [TestClass]
+    public class CircuitBreakerShould
+    {
+        int _FailLimit = 2;
+        CircuitBreaker _CircuitBreaker;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _CircuitBreaker = new CircuitBreaker(_FailLimit);
+        }
+
+        [TestMethod]
+        public void ExecuteActionLikeNormal()
+        {
+            var wasExecuted = false;
+
+            _CircuitBreaker.ExecuteAction(() =>
+            {
+                wasExecuted = true;
+            });
+
+            Assert.IsTrue(wasExecuted);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void ThrowActionsException()
+        {
+            _CircuitBreaker.ExecuteAction(() =>
+            {
+                var zero = 0;
+                zero = zero / zero;
+            });
+        }
+
+        [TestMethod]
+        public void ThrowCircuitBreakerOpenExceptionWhenActionFailsOverFailLimit()
+        {
+            Action divideByZeroAction = () =>
+                {
+                    var zero = 0;
+                    zero = zero / zero;
+                };
+            for (var i = 0; i < (_FailLimit - 1); i++)
+            {
+                try
+                {
+                    _CircuitBreaker.ExecuteAction(divideByZeroAction);
+                }
+                catch (DivideByZeroException) { }
+            }
+
+            try
+            {
+                _CircuitBreaker.ExecuteAction(divideByZeroAction);
+                Assert.Fail("Should have thrown CircuitBreakerOpenException");
+            }
+            catch (CircuitBreakerOpenException exception)
+            {
+                Assert.IsInstanceOfType(exception.LastExceptionThatTrippedCircuit, typeof(DivideByZeroException));
+            }
+        }
+
+        [TestMethod]
+        public void ThrowCircuitBreakerOpenExceptionWithoutExecutingActionWhenCircuitOpen()
+        {
+            var executed = false;
+
+            Action divideByZeroAction = () =>
+            {
+                var zero = 0;
+                zero = zero / zero;
+            };
+            for (var i = 0; i < (_FailLimit); i++)
+            {
+                try
+                {
+                    _CircuitBreaker.ExecuteAction(divideByZeroAction);
+                }
+                catch (DivideByZeroException) { }
+                catch (CircuitBreakerOpenException exception)
+                {
+                    Assert.IsInstanceOfType(exception.LastExceptionThatTrippedCircuit, typeof(DivideByZeroException));
+                }
+            }
+
+            try
+            {
+                _CircuitBreaker.ExecuteAction(() => executed = true);
+                Assert.Fail("Should have thrown CircuitBreakerOpenException");
+            }
+            catch (CircuitBreakerOpenException) { }
+
+            Assert.IsFalse(executed);
+        }
+    }
+}
